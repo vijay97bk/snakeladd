6cf3cf67df43edb7b5299d32fbb037ca64e85a7a
Vijay <vijay97bk@gmail.com>
2020-12-17 11:27:02 +0530

    uc2 updated

--- snakeladd.sh
+++ snakeladd.sh
@@ -1,7 +1,15 @@
 #!/bin/bash/ -x
 #set all pos
-player1=0
-startpos=0
-dice=$((RANDOM%6+1))
+start () {
+echo welcome to snake and ladder game
+Players=1
+StartPos=0
+}
+start
 
+rolldice () {
+dice=$((RANDOM%6+1))
+echo dice value $dice
+}
+rolldice
 
