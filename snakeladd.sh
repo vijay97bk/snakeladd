516ae6555431685c400253625df47571d5e04ec1
Vijay <vijay97bk@gmail.com>
2020-12-17 13:01:38 +0530

    UC4 added

--- snakeladd.sh
+++ snakeladd.sh
@@ -1,36 +1,47 @@
 #!/bin/bash/ -x
 #set all pos
 start () {
-echo welcome to snake and ladder game
-Players=1
-StartPos=0
-PlayerPos=0
+	echo welcome to snake and ladder game
+	Players=1
+	StartPos=0
+	PlayerPos=0
+	FinalPos=100
 }
 start
 
+
 rolldice () {
-Dice=$((RANDOM%6+1))
-echo dice value $Dice
-}
+	Dice=$((RANDOM%6+1))
+	echo dice value $Dice
+	}
 rolldice
 
+
 CheckOptions () {
-Opt=$((RANDOM%3))
-if [ $Opt -eq 0 ]
-then
-echo no play
-echo your position is $PlayerPos
-elif [ $Opt -eq 1 ]
-then
-PlayerPos=($PlayerPos - $Dice)
-echo snake
-echo your position is $PlayerPos
-elif [ $Opt -eq 2 ]
-then
-PlayerPos=($PlayerPos + $Dice)
-echo Ladder
-echo your position is $PlayerPos
-fi
+while [ $PlayerPos -le $FinalPos ]
+do
+	Opt=$((RANDOM%3))
+	if [ $Opt -eq 0 ]
+	then
+		echo no play
+
+	elif [ $Opt -eq 1 ]
+	then
+		((PlayerPos -= $Dice))
+		echo snake
+
+	elif [ $Opt -eq 2 ]
+	then
+		((PlayerPos += $Dice))
+		echo Ladder
+	fi
+
+	if [ $PlayerPos -lt 0 ]
+	then
+	PlayerPos=0
+	fi
+echo Player Pos is $PlayerPos
+done
 }
 rolldice
 start
