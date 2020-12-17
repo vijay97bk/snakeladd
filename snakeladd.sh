10683e787742974503948642c350fd71d98213fb
Vijay <vijay97bk@gmail.com>
2020-12-17 12:41:55 +0530

    UC3 added

--- snakeladd.sh
+++ snakeladd.sh
@@ -3,35 +3,37 @@
 start () {
 echo welcome to snake and ladder game
 Players=1
-StartPos=0
+START_POS=0
 PlayerPos=0
+rolldice
+C
 }
 start
 
 rolldice () {
-Dice=$((RANDOM%6+1))
-echo dice value $Dice
+	Dice=$((RANDOM%6+1))
+	echo dice value $Dice
 }
 rolldice
 
 CheckOptions () {
+
 Opt=$((RANDOM%3))
-if [ $Opt -eq 0 ]
-then
-echo no play
-echo your position is $PlayerPos
-elif [ $Opt -eq 1 ]
-then
-PlayerPos=($PlayerPos - $Dice)
-echo snake
-echo your position is $PlayerPos
-elif [ $Opt -eq 2 ]
-then
-PlayerPos=($PlayerPos + $Dice)
-echo Ladder
-echo your position is $PlayerPos
-fi
+	if [ $Opt -eq 0 ]
+	then
+		echo no play
+		echo your position is $PlayerPos
+
+	elif [ $Opt -eq 1 ]
+	then
+		((PlayerPos -= $Dice))
+		echo snake
+		echo your position is $PlayerPos
+	elif [ $Opt -eq 2 ]
+	then
+		((PlayerPos+=$Dice))
+		echo Ladder
+		echo your position is $PlayerPos
+	fi
 }
-rolldice
-start
 CheckOptions
